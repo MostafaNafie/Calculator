d5b27a1fb71f549ddd34562f277c1bd2e371bfe4
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2020-05-07 21:58:08 +0200

    Add result label

--- Calculator/View/MainView.swift
+++ Calculator/View/MainView.swift
@@ -10,14 +10,36 @@ import UIKit
 
 class MainView: UIView {
 	
+	private let resultLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "Result"
+		label.font = .systemFont(ofSize: 30)
+		return label
+	}()
 	
 }
 
 // MARK: - Helper functions
 
 extension MainView {
+	
 	func setupView() {
-
+		setupUI()
+		setupLayout()
 	}
+	
+	private func setupUI() {
+		addSubview(resultLabel)
+	}
+	
+	private func setupLayout() {
+		// Result Label
+		NSLayoutConstraint.activate([
+			resultLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+			resultLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+		])
+	}
+	
 }
 
