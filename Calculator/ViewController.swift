f4a61b232fddc3f51d40f3a1f1bba30c8d9f1f39
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2020-05-07 21:19:14 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Mustafa on 7/5/20.
+//  Copyright © 2020 Mustafa Nafie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
